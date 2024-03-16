eb749b9736fea96ff7de302c3a427104ca6cac88
Koirannaama <ollimatti.saarinen@gmail.com>
2024-03-16 19:28:03 +0200

    Ottelutaulukot joukkuenäkymässä

--- src/app/teams-view/teams-view/teams-view.component.ts
+++ src/app/teams-view/teams-view/teams-view.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { TeamsStateService } from '../services/teams-state.service';
 
 @Component({
-  selector: 'app-teams-view',
-  templateUrl: './teams-view.component.html',
-  styleUrls: ['./teams-view.component.scss']
+    selector: 'app-teams-view',
+    templateUrl: './teams-view.component.html',
+    styleUrls: ['./teams-view.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TeamsViewComponent {
+    @HostBinding('class') public class = 'view-component';
 
+    constructor(public readonly stateService: TeamsStateService) {}
+
+    public selectTeam(team: string): void {
+        this.stateService.setTeam(team);
+    }
 }
