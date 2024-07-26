c1464bdc89f1bdd6bcc48aa52bfd0f8863685718
Koirannaama <ollimatti.saarinen@gmail.com>
2024-07-26 20:41:02 +0300

    Material v17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import { TeamsViewModule } from './teams-view/teams-view.module';
+import { MAT_BUTTON_TOGGLE_DEFAULT_OPTIONS } from '@angular/material/button-toggle';
 
 @NgModule({
     declarations: [
@@ -21,7 +22,12 @@ import { TeamsViewModule } from './teams-view/teams-view.module';
         NavBarComponent,
         TeamsViewModule,
     ],
-    providers: [],
+    providers: [
+        {
+            provide: MAT_BUTTON_TOGGLE_DEFAULT_OPTIONS,
+            useValue: { hideMultipleSelectionIndicator: true, hideSingleSelectionIndicator: true },
+        }
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule { }
